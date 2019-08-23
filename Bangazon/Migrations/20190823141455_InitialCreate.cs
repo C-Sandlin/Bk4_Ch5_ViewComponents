e518018cb9b95501140afaddc053848463d6d4aa
Colin Sandlin <csandlin010@gmail.com>
2019-08-23 08:30:04 -0700

    finished exercise

---
+++ Bangazon/Migrations/20190823141455_InitialCreate.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Bangazon.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "AspNetUsers",
+                keyColumn: "Id",
+                keyValue: "00000000-ffff-ffff-ffff-ffffffffffff",
+                columns: new[] { "ConcurrencyStamp", "PasswordHash" },
+                values: new object[] { "e9fdd54b-4435-4b62-b19d-5de43d1172b5", "AQAAAAEAACcQAAAAEPfgRo6MTg1+7ajAd1NVBIqfBm2UK6ykcRv/5NFrsHZSS0idFYWtwEPs0h2cCjntqw==" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "AspNetUsers",
+                keyColumn: "Id",
+                keyValue: "00000000-ffff-ffff-ffff-ffffffffffff",
+                columns: new[] { "ConcurrencyStamp", "PasswordHash" },
+                values: new object[] { "e9b8b50d-023c-4aee-a71f-58a3de88e038", "AQAAAAEAACcQAAAAEF6xFBqJnrP0RsWyisTUL22FkuJDoeHsOlojKYHClpAShlq6uJhB6ERZawSUs1hyVg==" });
+        }
+    }
+}
---
+++ Bangazon/ViewComponents/ProductCountViewComponent.cs
@@ -0,0 +1,46 @@
+﻿using Bangazon.Data;
+using Bangazon.Models;
+using Bangazon.Models.ProductViewModels;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Bangazon.ViewComponents
+{
+    public class ProductCountViewComponent : ViewComponent
+    {
+        private readonly ApplicationDbContext _context;
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public ProductCountViewComponent(
+            ApplicationDbContext context,
+            UserManager<ApplicationUser> userManager)
+        {
+            _context = context;
+            _userManager = userManager;
+        }
+
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var viewModel = new ProductCountViewModel();
+
+            var user = await _userManager.GetUserAsync(HttpContext.User);
+
+            if (user == null)
+            {
+                //because default integer is zero, just return the viewModel without adding to product count if not logged in
+                return View(viewModel);
+            }
+
+            var productCount = _context.Product.Where(prod => prod.UserId == user.Id).Count();
+
+            viewModel.ProductCount = productCount;
+
+            return View(viewModel);
+        }
+    }
+}
